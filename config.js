89b481040b15f52c243b927913208ee7e644f47e
Test <deimudda@gmail.com>
2024-09-21 01:58:55 +0200

    del

--- config.js
+++
@@ -1,8 +0,0 @@
-const TOKENS = [
-    { mint: 'So11111111111111111111111111111111111111112', ticker: 'SOL' },
-    { mint: 'Es9vMFrzaCERz1k1V1YQz1k1V1YQz1k1V1YQz1k1V1YQ', ticker: 'USDC' },
-    { mint: '2o1vMFrzaCERz1k1V1YQz1k1V1YQz1k1V1YQz1k1V1YQ', ticker: 'USDT' },
-    // Add more tokens as needed
-  ];
-  
-  module.exports = { TOKENS };
